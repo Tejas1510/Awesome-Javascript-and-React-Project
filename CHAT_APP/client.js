8fce6d4de3c14f89a003b0a2fce0d03ef22b149b
Sushantrimurti <56467504+Sushantrimurti@users.noreply.github.com>
2020-10-02 19:43:09 +0530

    RealTime Chat App added

---
+++ CHAT_APP/client.js
@@ -0,0 +1,40 @@
+const socket = io('http://localhost:8000');
+
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp');
+const messageContainer = document.querySelector(".container");
+var audio = new Audio('Ding-Sound-Effect.mp3');
+
+ const append = (message,position)=>{
+     const messageElement = document.createElement('div');
+     messageElement.innerText = message;
+     messageElement.classList.add('message');
+     messageElement.classList.add(position);
+     messageContainer.append(messageElement);
+     if(position == 'left'){
+         audio.play();
+     }
+ }
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`You: ${message}`,'right');
+    socket.emit('send',message);
+    messageInput.value = ''
+})
+
+ const name = prompt("Enter your name to join");
+ socket.emit('new-user-joined',name);
+
+ socket.on('user-joined',name =>{
+     append(`${name} joined the chat`,'right')
+ })
+
+ socket.on('receive',data =>{
+     append(`${data.name}: ${data.message}`,'left')
+ })
+
+ socket.on('left',name =>{
+     append(`${name} left the chat`, 'right')
+ })

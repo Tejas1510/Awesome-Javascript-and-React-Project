c092737e6873fde331515db87f75234226148368
Jinesh Parakh <50925060+jineshparakh@users.noreply.github.com>
2020-10-03 09:45:07 +0530

    Add js

---
+++ Blogify/app/static/js/main.js
@@ -0,0 +1,17 @@
+const options = {
+    bottom: '64px', // default: '32px'
+    right: '32px', // default: '32px'
+    left: 'unset', // default: 'unset'
+    time: '2s', // default: '0.3s'
+    mixColor: '#fff', // default: '#fff'
+    backgroundColor: '#fff',  // default: '#fff'
+    buttonColorDark: '#100f2c',  // default: '#100f2c'
+    buttonColorLight: '#fff', // default: '#fff'
+    saveInCookies: true, // default: true,
+    label: '🌓', // default: ''
+    autoMatchOsTheme: true // default: true
+  }
+  
+  const darkmode = new Darkmode(options);
+  console.log("LOL")
+  darkmode.showWidget();

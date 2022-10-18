a921627ca5cbeda1023cb43a347eaa600cb9458a
Anjali Prasad <riseup.anjali07@gmail.com>
2022-10-18 13:48:31 +0530

    first commit

---
+++ List of latest netflix series/src/sdata.jsx
@@ -0,0 +1,39 @@
+const sdata=[
+    {
+        sname:"DARK ",
+        imgsrc:" https://flxt.tmsimg.com/assets/p14652158_b_v13_az.jpg",
+        title:" A original Netflix Series",
+        links: "https://www.netflix.com/in/title/80100172 ",
+    },
+    {
+        sname:" MONEY HEIST",
+        imgsrc:"https://m.media-amazon.com/images/M/MV5BNDJkYzY3MzMtMGFhYi00MmQ4LWJkNTgtZGNiZWZmMTMxNzdlXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_FMjpg_UX1000_.jpg ",
+        title:"A original Netflix Series ",
+        links: "https://www.netflix.com/in/title/80192098 ",
+    },
+    {
+        sname:"KOTA FACTORY ",
+        imgsrc:" https://doms2cents.com/wp-content/uploads/2021/09/MV5BODljNDYzZDgtMDc1NS00YzE1LTg1NGMtYTAyOWYwZWYyZDk3XkEyXkFqcGdeQXVyMTI1NDEyNTM5._V1_.jpg",
+        title:"A original Netflix Series ",
+        links: "https://www.netflix.com/in/title/81249783 ",
+    },
+    {
+        sname:" MISMATCHED",
+        imgsrc:"https://m.media-amazon.com/images/M/MV5BOTUzMTMzNTgtNmI5NS00ZTI1LTgzMjYtZTFkZWZkYTE0MWMyXkEyXkFqcGdeQXVyNjY1MTg4Mzc@._V1_.jpg ",
+        title:"A original Netflix Series ",
+        links: "https://www.netflix.com/in/title/80220079 ",
+    },
+    {
+        sname:"OZARK",
+        imgsrc:"https://flxt.tmsimg.com/assets/p14303628_b_v13_aa.jpg",
+        title:"A original Netflix Series",
+        links:" https://www.netflix.com/in/title/80117552",
+    },
+    {
+        sname:"LUCIFER",
+        imgsrc:"https://m.media-amazon.com/images/M/MV5BNDJjMzc4NGYtZmFmNS00YWY3LThjMzQtYzJlNGFkZGRiOWI1XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg",
+        title:"A original Netflix Series",
+        links:"https://www.netflix.com/in/title/80057918 ",
+    }
+];
+export default sdata;
